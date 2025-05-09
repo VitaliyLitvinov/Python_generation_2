27b989c6bd862fd7424966eaa03395132263745d
Vitaly_NB <v.litvinov2000@gmail.com>
2025-05-09 11:32:48 +0300

    om

--- web_application.py
+++ web_application.py
@@ -1,25 +1,14 @@
+from flask import Flask, request
 import os
 
-# A simple web application using Flask
-from flask import Flask, jsonify
-
-# Create a Flask application
 app = Flask(__name__)
 
-# Define a route for the root URL
 @app.route('/')
 def home():
-    host_name = os.uname()[1]  # Get the host name
-    ip_address = os.popen('hostname -I').read().strip()  # Get the IP address
-    author = os.getenv('AUTHOR', 'Unknown')  # Get the author from environment variable
-    return f"Welcome to the Simple Web Application!\nHost Name: {host_name}\nIP Address: {ip_address}\nAuthor: {author}"
+    host_name = request.host
+    host_ip = request.remote_addr
+    author = os.getenv('AUTHOR', 'Неизвестный автор')
+    return f'<html><body><h1>Информация о сервере</h1><p>Имя хоста: {host_name}</p><p>IP адрес: {host_ip}</p><p>Автор: {author}</p></body></html>'
 
-# Define a route for a JSON response
-@app.route('/api/data')
-def get_data():
-    data = {'message': 'Hello, World!', 'status': 'success'}
-    return jsonify(data)
-
-# Run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000)
